fcd4f82e108969f8bc78fe4b3d19570d6ddce9e2
danyzhr <danyzhr@live.fr>
2022-11-27 21:06:17 +0100

    add delete session button

--- CoachNotify/app/src/main/java/com/namelesscloudco/coachnotify/GetAPI.kt
+++ CoachNotify/app/src/main/java/com/namelesscloudco/coachnotify/GetAPI.kt
@@ -1,11 +1,16 @@
 package com.namelesscloudco.coachnotify
 
 import retrofit2.Call
+import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
 
 
 interface GetAPI {
-    @PUT("/notification/save_session/{coachId}")
+    @POST("/notification/save-session/{coachId}")
     fun notify(@Path("coachId") coachId: String) : Call<String>
+
+    @POST("/notification/del-session/{coachId}")
+    fun delete(@Path("coachId") coachId: String) : Call<String>
+
 }
--- CoachNotify/app/src/main/java/com/namelesscloudco/coachnotify/MainActivity.kt
+++ CoachNotify/app/src/main/java/com/namelesscloudco/coachnotify/MainActivity.kt
@@ -12,11 +12,13 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     lateinit var editText:EditText
     lateinit var button: Button
+    lateinit var button2: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button = findViewById(R.id.button)
+        button2 = findViewById(R.id.button2)
         editText = findViewById(R.id.editText)
         button.setOnClickListener {
             val name = editText.text
@@ -27,7 +29,23 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Error!", Toast.LENGTH_LONG).show()
             }
         }
+        button2.setOnClickListener {
+            val name = editText.text
+            try {
+                notifyAbsence(name.toString())
+                Toast.makeText(this, "Success!", Toast.LENGTH_LONG).show()
+            } catch (e:java.lang.Exception) {
+                Toast.makeText(this, "Error!", Toast.LENGTH_LONG).show()
+            }
+        }
     }
+
+    private fun notifyAbsence(name: String) {
+        Log.d("notify", "Notify absence coach : $name")
+        val request: retrofit2.Call<String> = RetrofitHelper.getInstance().create(GetAPI::class.java).delete(name)
+        request.execute()
+    }
+
     private fun notifyPresence(name: String) {
         Log.d("notify", "Notify presence coach : $name")
        val request: retrofit2.Call<String> = RetrofitHelper.getInstance().create(GetAPI::class.java).notify(name)
