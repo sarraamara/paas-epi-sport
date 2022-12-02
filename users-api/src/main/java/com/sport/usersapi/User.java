e98641d8f4a04d993f0b6bc5ce20723950751f9c
danyzhr <danyzhr@live.fr>
2022-12-02 15:43:47 +0100

    add user model and database connection

--- users-api/src/main/java/com/sport/usersapi/User.java
+++ users-api/src/main/java/com/sport/usersapi/User.java
@@ -1,9 +1,92 @@
 package com.sport.usersapi;
 
 
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+
+@Entity
+@Table(name = "userProfile")
 public class User {
+
     @Id
-    String userId;
+    @Column(name = "userId", nullable = false)
+    int userId;
+
+    @Column(name = "lastname")
+    String lastname;
+
+    @Column(name = "firstname")
+    String firstname;
+
+    @Column(name = "age")
+    int age;
+
+    @Column(name = "weight")
+    int weight;
+
+    @Column(name = "height")
+    int height;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", lastname='" + lastname + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                ", height=" + height +
+                '}';
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
