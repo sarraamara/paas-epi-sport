0447424cb8f602793787fb32c72e38e3dea72f6d
danyzhr <danyzhr@live.fr>
2023-04-22 21:58:41 +0200

    add listener on collector for debug

--- heart-rate-sensor-data-collector/src/main/java/com/sport/heartratesensordatacollector/service/HeartRateService.java
+++ heart-rate-sensor-data-collector/src/main/java/com/sport/heartratesensordatacollector/service/HeartRateService.java
@@ -1,7 +1,11 @@
 package com.sport.heartratesensordatacollector.service;
 
-import com.sport.heartratesensordatacollector.model.UserHeartRate;
+import com.sport.heartratesensordatacollector.controller.HeartRateController;
+import com.sport.heartratesensordataworker.model.User;
+import com.sport.heartratesensordataworker.model.UserHeartRate;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,13 +15,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class HeartRateService {
+    private static final Logger logger = LoggerFactory.getLogger(HeartRateController.class);
 
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
     @KafkaListener(topics = "hrdata-topic", groupId = "ncc")
     public void listen(ConsumerRecord<String, Object> record) {
+        logger.info("GETTING NEW MESSAGE");
         // Traitement du message reçu
-        System.out.println(record.value());
+        logger.info(record.value().toString());;
     }
 
 
