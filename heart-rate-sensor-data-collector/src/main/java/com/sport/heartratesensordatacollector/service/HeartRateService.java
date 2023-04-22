d786fbb81a3910dc3a29d55f06365c82dd80524b
danyzhr <danyzhr@live.fr>
2023-04-22 22:07:58 +0200

    change userheartrate class used

--- heart-rate-sensor-data-collector/src/main/java/com/sport/heartratesensordatacollector/service/HeartRateService.java
+++ heart-rate-sensor-data-collector/src/main/java/com/sport/heartratesensordatacollector/service/HeartRateService.java
@@ -1,14 +1,10 @@
 package com.sport.heartratesensordatacollector.service;
 
 import com.sport.heartratesensordatacollector.controller.HeartRateController;
-import com.sport.heartratesensordataworker.model.User;
-import com.sport.heartratesensordataworker.model.UserHeartRate;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
