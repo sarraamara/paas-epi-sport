15e5445abeb099b736b7cb85957d7c9e40f7b98f
danyzhr <danyzhr@live.fr>
2023-04-23 23:41:09 +0200

    add repository annotation

--- heart-rate-sensor-data-worker/src/main/java/com/sport/heartratesensordataworker/repository/HeartRateRepository.java
+++ heart-rate-sensor-data-worker/src/main/java/com/sport/heartratesensordataworker/repository/HeartRateRepository.java
@@ -2,6 +2,8 @@ package com.sport.heartratesensordataworker.repository;
 
 import com.sport.common.model.UserHeartRate;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HeartRateRepository extends MongoRepository<UserHeartRate, String> {
 }
