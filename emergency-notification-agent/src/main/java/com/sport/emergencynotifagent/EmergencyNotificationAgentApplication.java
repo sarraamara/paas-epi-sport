b71bac3ae7fd1504cea1fa1dedc4d806737d8acb
Sarra Amara <sarra.amara@icloud.com>
2023-04-23 23:19:32 +0200

    fix repository dependency

--- emergency-notification-agent/src/main/java/com/sport/emergencynotifagent/EmergencyNotificationAgentApplication.java
+++ emergency-notification-agent/src/main/java/com/sport/emergencynotifagent/EmergencyNotificationAgentApplication.java
@@ -2,8 +2,11 @@ package com.sport.emergencynotifagent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.sport.common.model"})
 public class EmergencyNotificationAgentApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmergencyNotificationAgentApplication.class, args);
